19b840e06bd5f437df7417aa3a8f4defb8ba3fcd
hemanth1110 <chowdary.hemanth.tummala@gmail.com>
2021-05-23 21:04:25 +0530

    website

---
+++ college space/javascript/daa.js
@@ -0,0 +1,10 @@
+var unit1 = ["https://www.youtube.com/embed/hNkOCH29DOw","https://www.youtube.com/embed/1VDdR2elRrM","https://www.youtube.com/embed/ZfkSnaFkPj8","https://www.youtube.com/embed/5BKWvFyXfQY","https://www.youtube.com/embed/5BKWvFyXfQY","https://www.youtube.com/embed/wTk38yIkehs","https://www.youtube.com/embed/wTk38yIkehs","https://www.youtube.com/embed/YP-yH5DvkT0"];
+var videoFrame = document.getElementsByTagName('iframe')[0];
+var links = document.getElementsByClassName('slinks');
+var lenght = links.length;
+for(var i=0; i<lenght; i++){
+    links[i].addEventListener("click",function(){
+        var num = parseInt(this.getAttribute("value"));
+        videoFrame.setAttribute("src",unit1[num-1]);
+    });
+}
