c6608c013955f892da68b2ad7901364c8cf19fa2
hemanth1110 <79350908+hemanth1110@users.noreply.github.com>
2021-05-24 21:18:28 +0530

    Update daa.js

--- daa.js
+++ daa.js
@@ -1,4 +1,4 @@
-var unit1 = ["https://www.youtube.com/embed/hNkOCH29DOw","https://www.youtube.com/embed/1VDdR2elRrM","https://www.youtube.com/embed/ZfkSnaFkPj8","https://www.youtube.com/embed/5BKWvFyXfQY","https://www.youtube.com/embed/5BKWvFyXfQY","https://www.youtube.com/embed/wTk38yIkehs","https://www.youtube.com/embed/wTk38yIkehs","https://www.youtube.com/embed/YP-yH5DvkT0"];
+var unit1 = ["https://www.youtube.com/embed/hNkOCH29DOw","https://www.youtube.com/embed/1VDdR2elRrM","https://www.youtube.com/embed/ZfkSnaFkPj8","https://www.youtube.com/embed/5BKWvFyXfQY","https://www.youtube.com/embed/5BKWvFyXfQY","https://www.youtube.com/embed/wTk38yIkehs","https://www.youtube.com/embed/SJ2eFEK0wTQ","https://www.youtube.com/embed/YP-yH5DvkT0"];
 var videoFrame = document.getElementsByTagName('iframe')[0];
 var links = document.getElementsByClassName('slinks');
 var lenght = links.length;
